18237cf761076bd3796231a60ebb7a177873f879
joao tiago viegas <jtviegas.js@gmail.com>
2023-10-30 11:50:36 +0100

    initial commit

---
+++ .mysys/bin/mysys.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+# ===> COMMON SECTION START  ===>
+
+# http://bash.cumulonim.biz/NullGlob.html
+shopt -s nullglob
+# -------------------------------
+this_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+if [ -z "$this_folder" ]; then
+  this_folder=$(dirname $(readlink -f $0))
+fi
+mysys_folder=$(dirname "$this_folder")
+# -------------------------------
+debug(){
+    local __msg="$1"
+    echo " [DEBUG] `date` ... $__msg "
+}
+
+info(){
+    local __msg="$1"
+    echo " [INFO]  `date` ->>> $__msg "
+}
+
+warn(){
+    local __msg="$1"
+    echo " [WARN]  `date` *** $__msg "
+}
+
+err(){
+    local __msg="$1"
+    echo " [ERR]   `date` !!! $__msg "
+}
+
+# ---------- CONSTANTS ----------
+export FILE_VARIABLES=${FILE_VARIABLES:-".variables"}
+export FILE_SECRETS=${FILE_SECRETS:-".secrets"}
+export MYSYS_PATH=~/.mysys
+export TAR_FILE="mysys.tar.bz2"
+
+if [ "$MYSYS_PATH" != "$mysys_folder" ]; then
+    err "$MYSYS_PATH is not identical to $mysys_folder ! sorry I am leaving"
+    exit 1
+fi
+
+# -------------------------------
+if [ ! -f "$mysys_folder/$FILE_VARIABLES" ]; then
+  warn "we DON'T have a $FILE_VARIABLES variables file - creating it"
+  touch "$mysys_folder/$FILE_VARIABLES"
+else
+  . "$mysys_folder/$FILE_VARIABLES"
+fi
+
+if [ ! -f "$mysys_folder/$FILE_SECRETS" ]; then
+  warn "we DON'T have a $FILE_SECRETS secrets file - creating it"
+  touch "$mysys_folder/$FILE_SECRETS"
+else
+  . "$mysys_folder/$FILE_SECRETS"
+fi
+
+# ---------- FUNCTIONS ----------
+
+update(){
+  echo "[update] ..."
+
+  _pwd=`pwd`
+
+  if [ ! -d "$MYSYS_PATH" ]; then
+    err "can't find $MYSYS_PATH folder ! sorry I am leaving"
+    exit 1
+  fi
+
+  cd "$MYSYS_PATH"
+
+  curl -s https://api.github.com/repos/jtviegas-sandbox/mysys/releases/latest \
+  | grep "browser_download_url.*mysys\.tar\.bz2" \
+  | cut -d '"' -f 4 | wget -qi -
+  tar xjpvf $TAR_FILE
+  if [ ! "$?" -eq "0" ] ; then echo "[update_bashutils] could not untar it" && cd "$_pwd" && return 1; fi
+  rm $TAR_FILE
+
+  cd "$_pwd"
+  echo "[update] ...done."
+}
+
+package(){
+  info "[package] ..."
+  _pwd=`pwd`
+  cd "$MYSYS_PATH"
+
+  tar cjpvf "$TAR_FILE" "include" "bin"
+  if [ ! "$?" -eq "0" ] ; then err "[package] could not tar it" && cd "$_pwd" && return 1; fi
+
+  cd "$_pwd"
+  info "[package] ...done."
+}
+
+
+# -------------------------------------
+usage() {
+  cat <<EOM
+  usage:
+  $(basename $0) { command }
+
+    commands:
+      - update: updates 'mysys'
+      - package: packages mysys into a tar for release purposes
+EOM
+  exit 1
+}
+
+debug "1: $1 2: $2 3: $3 4: $4 5: $5 6: $6 7: $7 8: $8 9: $9"
+
+case "$1" in
+  update)
+    update
+    ;;
+  package)
+    package
+    ;;
+  *)
+    usage
+    ;;
+esac
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # mysys
 configurations for my system (laptop)
+
+## getting started
+
+* create `~/.mysys/` folder:
+    ```
+    mkdir ~/.mysys
+    ```
+* __source__ the `.mysys/include` file in your system's profile initialisation file ( `.zprofile`, `.bash_profile`, etc... )
+    ```
+    . ~/.mysys/include
+    ```
+
+* download `mysys.sh` script
+  * ... from `~/.mysys/` folder
+    ```
+    cd ~/.mysys/
+    wget https://raw.githubusercontent.com/jtviegas-sandbox/mysys/main/.mysys/bin/mysys.sh -o bin/mysys.sh
+    ```
+  * make it executable: `chmod +x bin/mysys.sh`
+  * update bashutils include file: `bin/mysys.sh update`
+
+you should now see this when running `bin/mysys.sh`:
+
+    [WARN]  Mon Oct 30 11:46:50 CET 2023 *** we DON'T have a .variables variables file - creating it 
+    [WARN]  Mon Oct 30 11:46:50 CET 2023 *** we DON'T have a .secrets secrets file - creating it 
+    [DEBUG] Mon Oct 30 11:46:50 CET 2023 ... 1:  2:  3:  4:  5:  6:  7:  8:  9:  
+    usage:
+    mysys.sh { command }
+
+        commands:
+        - update: updates 'mysys'
+        - package: packages mysys into a tar for release purposes
+
